1b05190ddb5b1a15310a6c59e1e62715be9df826
Ranjit <ranjitkr87@gmail.com>
2021-11-08 17:52:55 +0530

    dropdown menu

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,8 @@
 function dropdown(){
-    document.getElementById("myDropDown").style.display="block";
+    var x = document.getElementById("myDropDown");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
 }
